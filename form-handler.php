89d10e285736d21d87a8e4cf3824c4a537c90f07
ashwinPercy <ashwin.1701018@srec.ac.in>
2021-07-02 18:52:41 +0530

    fixed php bug

--- form-handler.php
+++ form-handler.php
@@ -1,24 +1,22 @@
 <?php
 $name = $_POST['name'];
-$visitors_email = $_POST['email'];
+$visitor_email = $_POST['email'];
 $service = $_POST['service'];
 $message = $_POST['message'];
 
-$email_from = 'client@ashtech.com';
+$email_from = 'client@tech-web-ashwinxeez.netlify.app';
 
 $email_subject= 'New query submission';
 
-$email_body = "User Name : $name .\n";
-                "User Email : $visitors_email.\n";
+$email_body =   "User Name : $name .\n";
+                "User Email : $visitor_email.\n";
                 "Service requested : $service .\n";
                 "Message : $message.\n";
 
 $to = 'aw1728@gmail.com';
 $headers = "from : $email_from \r \n";
-$headers .= "reply-to : $visitors_email \r \n";
+$headers .= "reply-to : $visitor_email \r \n";
 mail($to,$email_subject,$email_body,$headers);
 
-header("location. contact.html");
-
-
+header("Location: contact.html");
 ?>
