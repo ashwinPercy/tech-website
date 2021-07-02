8f444b5df31dd8cae120237a9be45c29a94e39c1
ashwinPercy <ashwin.1701018@srec.ac.in>
2021-07-02 19:19:35 +0530

    final commit

--- form-handler.php
+++ form-handler.php
@@ -1,22 +1,24 @@
 <?php
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
-$service = $_POST['service'];
+$subject = $_POST['subject'];
 $message = $_POST['message'];
 
 $email_from = 'client@tech-web-ashwinxeez.netlify.app';
 
 $email_subject= 'New query submission';
 
-$email_body =   "User Name : $name .\n";
-                "User Email : $visitor_email.\n";
-                "Service requested : $service .\n";
-                "Message : $message.\n";
+$email_body = "User Name : $name .\n".
+                "User Email: $visitor_email.\n". 
+                    "Subject : $subject.\n".
+                    "User Message: $message .\n";
+
 
 $to = 'aw1728@gmail.com';
-$headers = "from : $email_from \r \n";
-$headers .= "reply-to : $visitor_email \r \n";
+$headers = "From : $email_from \r \n";
+$headers .= "Reply-To : $visitor_email \r \n";
+
 mail($to,$email_subject,$email_body,$headers);
 
-header("Location: https://tech-web-ashwinxeez.netlify.app/contact.html");
+header("Location: index.html");
 ?>
